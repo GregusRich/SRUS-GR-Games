30e3ccd06dd47b3b08f56ecdf6554c9f5a18fc5b
GregusRich <141307499+GregusRich@users.noreply.github.com>
2023-09-06 14:37:25 +0800

    Class PlayerList - Added method for inserting a new node at the tail of the list.
    Class TestPlayerList
    - Updated the Unit tests to test this functionality for both empty and non-empty lists.

--- app/player_list.py
+++ app/player_list.py
@@ -35,3 +35,17 @@ class PlayerList:
             new_node._next = self.head  # Point the new node's next to the current head
             self.head._prev = new_node  # Point the current head's previous to the new node
             self.head = new_node  # Update the head to point to the new node
+
+    def insert_at_tail(self, new_node):
+        """
+        Inserts a new node at the tail of the list.
+
+        :param new_node: The new PlayerNode to insert.
+        """
+        if self.is_empty():
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node._prev = self.tail  # Point the new node's previous to the current tail
+            self.tail._next = new_node  # Point the current tail's next to the new node
+            self.tail = new_node  # Update the tail to point to the new node
--- test/player_list_test.py
+++ test/player_list_test.py
@@ -39,3 +39,39 @@ class TestPlayerList(unittest.TestCase):
         self.assertEqual(node2.next_node, node1)
         self.assertEqual(node1.prev_node, node2)
         self.assertIsNone(node2.prev_node)
+
+    def test_insert_at_tail_empty_list(self):
+        """
+        Test inserting a new node at the tail of an empty list.
+        """
+        player_list = PlayerList()
+        player = Player("1", "Alex")
+        node = PlayerNode(player)
+
+        player_list.insert_at_tail(node)
+
+        self.assertEqual(player_list.head, node)
+        self.assertEqual(player_list.tail, node)
+        self.assertIsNone(node.prev_node)
+        self.assertIsNone(node.next_node)
+
+    def test_insert_at_tail_non_empty_list(self):
+        """
+        Test inserting a new node at the tail of a non-empty list.
+        """
+        player_list = PlayerList()
+        player1 = Player("1", "Alex")
+        node1 = PlayerNode(player1)
+        player_list.insert_at_head(node1)
+
+        player2 = Player("2", "Bob")
+        node2 = PlayerNode(player2)
+
+        player_list.insert_at_tail(node2)
+
+        self.assertEqual(player_list.head, node1)
+        self.assertEqual(player_list.tail, node2)
+        self.assertEqual(node1.next_node, node2)
+        self.assertEqual(node2.prev_node, node1)
+        self.assertIsNone(node1.prev_node)
+        self.assertIsNone(node2.next_node)
